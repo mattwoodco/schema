67779c0454bcba1c453062f612de712512d08d39
mattwoodco <matt@mattwood.co>
2023-03-09 10:35:10 -0700

    Add ROADMAP.md

---
+++ ROADMAP.md
@@ -0,0 +1,32 @@
+**Field Types**
+
+- [ ] Geolocation
+- [ ] Yes / No / UNKNOWN
+- [ ] Temperature
+- [ ] Numerical Comparison
+- [ ] Dynamic collection
+- [ ] Autocomplete with source completion
+- [ ] Dynamic measurement collection
+- [ ] Radio Button
+- [ ] Temperature (w/ UNKNOWN)
+- [ ] Numerical mixture
+- [ ] Two-up Fuel
+- [ ] Note / Content
+- [ ] Confirm Button
+- [ ] Button to External Link
+
+**Schema Features**
+
+- [ ] Nested
+- [ ] Large, Composable Forms
+- [ ] Error Handling
+- [ ] Metadata
+- [ ] UUIDs for each field
+
+**Additional**
+
+- [ ] E2E Tests w/ Puppeteer
+- [ ] Display fields as flat list in a datatable
+- [ ] Add Airtable support
+- [ ] Additional UI Themes
+- [ ] Offline Support
--- prisma/migrations/20230308184552_/migration.sql
+++ prisma/migrations/20230308184552_/migration.sql
@@ -31,7 +31,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT,
     "email" TEXT,
-    "emailVerified" TIMESTAMP(3),
+    "emailSchemafied" TIMESTAMP(3),
     "passwordHash" TEXT,
     "image" TEXT,
     "role" TEXT NOT NULL DEFAULT 'GUEST',
@@ -41,7 +41,7 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "VerificationToken" (
+CREATE TABLE "SchemaficationToken" (
     "identifier" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL
@@ -121,10 +121,10 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_preferencesId_key" ON "User"("preferencesId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+CREATE UNIQUE INDEX "SchemaficationToken_token_key" ON "SchemaficationToken"("token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
+CREATE UNIQUE INDEX "SchemaficationToken_identifier_token_key" ON "SchemaficationToken"("identifier", "token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Preferences_userId_key" ON "Preferences"("userId");
